93214f738ef4874fca3804c87bff9b2210a2b9d5
ayushsarode <ayushsarode777@gmail.com>
2025-02-15 14:24:38 +0530

    meta line extra

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
 
 import type { Metadata } from "next";
-
 import {  Geist_Mono } from "next/font/google";
-
-
 import "./globals.css";
-
 import type React from "react"
-import type { Metadata } from "next"
+
 import { Inter } from "next/font/google"
 import "./globals.css"
 
