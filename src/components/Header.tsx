326c26ecbace81abb8f62784715bba9570729bab
Soham Khedkar <134027325+soham-khedkar@users.noreply.github.com>
2025-02-16 03:44:01 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,36 @@
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-
+"use client";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useAuthStore } from "@/store/authStore";
 export function Header() {
+  const { user, role, logout } = useAuthStore();
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-white/80 backdrop-blur-sm">
-      <div className="container bg-slate-100 flex items-center justify-between h-16 px-4 rounded-xl">
+    <header className="border-b">
+      <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         <div className="flex items-center gap-8">
           <Link href="/" className="flex items-center gap-2">
-            {/* <div className="w-8 h-8 bg-primary rounded-lg" /> */}
-            <span className="text-xl font-semibold">gethired.</span>
+            <div className="w-8 h-8 rounded-full bg-primary"></div>
+            <span className="text-xl font-semibold">JobHuntly</span>
           </Link>
           <nav className="hidden md:flex items-center gap-6">
-            <Link href="/jobs" className="text-xl text-muted-foreground hover:text-primary">
-              Find Jobs
-            </Link>
-            <Link href="/companies" className="text-xl text-muted-foreground hover:text-primary">
-              Browse Companies
-            </Link>
+            {role === "jobseeker" && (
+              <Link href="/findjobs" className="text-primary">
+                Find Jobs
+              </Link>
+            )}
+            {role === "recruiter" && (
+              <Link href="/add-job" className="text-muted-foreground">
+                Add Jobs
+              </Link>
+            )}
+            {role === "jobseeker" && (
+              <Link href="/companies" className="text-muted-foreground">
+                Browse Companies
+              </Link>
+            )}
           </nav>
         </div>
         <div className="flex items-center gap-4">
-
-          <Link href="/auth/login">
-          <Button variant="ghost" size="lg">
-            Login
-          </Button>
-          </Link>
-         <Link href="/auth/signup"> 
-          <Button size="lg">Sign Up</Button>
-          </Link>
           {user ? (
             <>
               <Link
@@ -43,7 +45,6 @@ export function Header() {
               <Link href={role === "jobseeker" ? "/dashboard/jobseeker/profile" : "/dashboard/recruiter/profile"}>
   <Button variant="ghost">Profile</Button>
 </Link>
-
               <Button
                 onClick={async () => {
                   await logout();
@@ -67,6 +68,5 @@ export function Header() {
         </div>
       </div>
     </header>
-  )
+  );
 }
-
